82ea5b7b4759745c95e6e4ae485ed1f3f4f2748d
“joanomana” <joansebastianomasu@gmail.com>
2025-03-10 17:23:19 -0500

    feat: :construction: Development menu

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,27 @@
+'use client';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Header from "@/components/Header";
 import Menu from "@/components/Menu";
 
 
+
+
 export default function Home() {
+  const [selectedPage, setSelectedPage] = useState("today");
+  const router = useRouter();
+
+
+
   return (
-    <div className="flex flex-col w-full h-dvh">
+    <div className="flex flex-col w-full h-dvh bg-[#F6EDFF] gap-4">
       <Header/>
-      <Menu/>
+      <Menu setSelectedPage={setSelectedPage} selectedPage={selectedPage}/>
+      <div className="flex flex-col px-10">
+        {selectedPage === "today" && <h1>Today</h1>}
+        {selectedPage === "tomorrow" && <h1>Tomorrow</h1>}
+        {selectedPage === "days" && <h1>10 days</h1>}
+      </div>
       
     </div>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,22 @@
+'use client';
+import React from "react";
+import { useRouter } from "next/navigation";
 
-
-export default function Menu() {
+export default function Menu({selectedPage, setSelectedPage}) {
     return (
-        <div>
-            <h1>Menu</h1>
+        <div className="flex justify-between items-center px-5">
+            <button 
+                className={`${selectedPage === "today" ? "bg-[#E0B6FF]  rounded-lg" : "bg-white"} py-2 px-6 rounded-lg `} onClick={()=>setSelectedPage("today")}>
+                Today
+            </button>
+            <button 
+                className={`${selectedPage === "tomorrow" ? "bg-[#E0B6FF]  rounded-lg" : "bg-white"} py-2 px-6 rounded-lg `} onClick={()=>setSelectedPage("tomorrow")}>
+                Tomorrow
+            </button>
+            <button 
+                className={`${selectedPage === "days" ? "bg-[#E0B6FF]  rounded-lg" : "bg-white"} py-2 px-6 rounded-lg `} onClick={()=>setSelectedPage("days")}>
+                10 days
+            </button>
+            
         </div>
     );}
